1ac97c1f7cb262738b58ad203e5e19353834dff4
NeneInTheTARDIS <miyavidechu@hotmail.com>
2018-05-08 21:22:07 +0200

    Removed hardcoded strings

--- app/src/main/java/com/example/android/justjava/MainActivity.java
+++ app/src/main/java/com/example/android/justjava/MainActivity.java
@@ -1,14 +1,19 @@
 package com.example.android.justjava;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.CheckBox;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.view.View;
+import android.widget.Toast;
 
 import java.text.NumberFormat;
 
 public class MainActivity extends AppCompatActivity {
-    public int quantity = 0;
+    public int quantity = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,36 +22,66 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void submitOrder(View view) {
-        int price = quantity * 5;
-        String priceMessage = "Total: $" + price;
-        priceMessage = priceMessage + "\nThank you!";
-        displayMessage(priceMessage);
+        EditText nameField = (EditText) findViewById(R.id.name_field);
+        String name = nameField.getText().toString();
+        CheckBox whippedCreamCheckbox = (CheckBox) findViewById(R.id.whipped_cream_checkbox);
+        boolean hasWhippedCream = whippedCreamCheckbox.isChecked();
+        CheckBox chocolateCheckbox = (CheckBox) findViewById(R.id.chocolate_checkbox);
+        boolean hasChocolate = chocolateCheckbox.isChecked();
+        int price = calculatePrice(hasWhippedCream, hasChocolate);
+
+        Intent intent = new Intent(Intent.ACTION_SENDTO);
+        intent.setData(Uri.parse("mailto:"));
+        intent.putExtra(Intent.EXTRA_SUBJECT, getString(R.string.subject, name));
+        intent.putExtra(Intent.EXTRA_TEXT, createOrderSummary(name, price, hasWhippedCream, hasChocolate));
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        }
     }
 
     public void increment(View view) {
-        quantity = quantity + 1;
-        display(quantity);
+        if(quantity < 100) {
+            quantity += 1;
+        } else {
+            Toast.makeText(getApplicationContext(), getString(R.string.more_than), Toast.LENGTH_SHORT).show();
+        }
+        displayQuantity(quantity);
     }
 
     public void decrement(View view) {
-        if (quantity > 0) {
-            quantity = quantity - 1;
+        if (quantity > 1) {
+            quantity -= 1;
+        } else {
+            Toast.makeText(getApplicationContext(), getString(R.string.less_than), Toast.LENGTH_SHORT).show();
         }
-        display(quantity);
+        displayQuantity(quantity);
     }
 
-    private void display(int number) {
+    private void displayQuantity(int numberOfCoffees) {
         TextView quantityTextView = (TextView) findViewById(R.id.quantity_text_view);
-        quantityTextView.setText("" + number);
+        quantityTextView.setText("" + numberOfCoffees);
     }
 
-    private void displayPrice(int number) {
-        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
-        priceTextView.setText(NumberFormat.getCurrencyInstance().format(number));
+    private int calculatePrice(boolean addWhippedCream, boolean addChocolate) {
+        int pricePerCup = 5;
+
+        if(addWhippedCream) {
+            pricePerCup += 1;
+        }
+        if(addChocolate) {
+            pricePerCup += 2;
+        }
+
+        return quantity * pricePerCup;
     }
 
-    private void displayMessage(String message) {
-        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
-        priceTextView.setText(message);
+    private String createOrderSummary(String name, int price, boolean addWhippedCream, boolean addChocolate){
+        String priceMessage = getString(R.string.order_summary_name, name);
+        priceMessage += "\n" + getString(R.string.order_summary_whipped_cream, addWhippedCream);
+        priceMessage += "\n" + getString(R.string.order_summary_chocolate, addChocolate);
+        priceMessage += "\n" + getString(R.string.order_summary_quantity, quantity);
+        priceMessage += "\n" + getString(R.string.order_summary_total, price);
+        priceMessage += "\n" + getString(R.string.order_summary_thank_you);
+        return priceMessage;
     }
 }
